aeb9042d8bf640ae3807e37b323a8e95d02bd98c
Reza Zeraat <rezacr588@gmail.com>
2021-08-12 06:25:53 +0430

    add qr code & ssearch icon & style

--- components/SearchBar.component.tsx
+++ components/SearchBar.component.tsx
@@ -1,10 +1,50 @@
-import React from "react"
-import { TextInput } from "react-native";
+import React from "react";
+import styled from "styled-components/native";
+import { FontAwesome } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const SearchInput = styled.View`
+  flex: 1;
+  padding: 10px;
+  background: #eef3f8;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  border-radius: 5px;
+`
+const Space = styled.View`
+  width: 10px;
+  height: 10px;
+`
+
+const Text = styled.Text`
+  color: rgba(0,0,0,0.6);
+  font-weight: 500;
+  font-size: 14px;
+`
+
+const LongSpace = styled.View`
+  flex: 1
+`
 
 export const SearchBar = () => {
-  const [text, onChangeText] = React.useState("Useless Text");
-  return <TextInput
-    onChangeText={onChangeText}
-    value={text}
-  />
+  return (
+    <SearchInput>
+      <FontAwesome
+        name="search"
+        size={15}
+        color="black"
+      />
+      <Space />
+      <Text>
+        Search
+      </Text>
+      <LongSpace />
+      <MaterialCommunityIcons
+        name="qrcode-scan"
+        size={20}
+        color="rgba(0,0,0,0.6)"
+      />
+    </SearchInput>
+  )
 }
--- navigation/components/header.component.tsx
+++ navigation/components/header.component.tsx
@@ -3,6 +3,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { ProfileImage } from "../../components/ProfileImage.component";
 import styled from 'styled-components/native'
 import { Ionicons } from '@expo/vector-icons';
+import { SearchBar } from "../../components/SearchBar.component";
 
 const Wrap = styled.View`
   padding: 5px 15px;
@@ -10,6 +11,13 @@ const Wrap = styled.View`
   display: flex;
   justify-content: space-between;
   flex-direction: row;
+  align-items: center;
+`
+const SearchWrapper = styled.View`
+  margin-left: 15px;
+  margin-right: 15px;
+  flex: 1;
+  height: 30px;
 `
 
 const Header = () => (
@@ -18,7 +26,10 @@ const Header = () => (
       <ProfileImage
         source="https://media-exp1.licdn.com/dms/image/D4D35AQHo6gsZcOlkWQ/profile-framedphoto-shrink_100_100/0/1628274394712?e=1628758800&v=beta&t=2A0jIq6SucqHGQApbIdVDIGLJnZ47T4GqBgOYeuI7JI"
       />
-      <Ionicons name="md-chatbox-ellipses" size={35} color="gray" />
+      <SearchWrapper>
+        <SearchBar />
+      </SearchWrapper>
+      <Ionicons name="md-chatbox-ellipses" size={25} color="rgba(0,0,0,0.6)" />
     </Wrap>
   </SafeAreaView>
 );
